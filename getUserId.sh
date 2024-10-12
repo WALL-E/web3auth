e874a16eede9f3324126821d56b119006eb88c57
wall-e <zz.zhangz@gmail.com>
2024-10-12 17:28:46 +0800

    release version 0.4

--- getUserId.sh
+++ getUserId.sh
@@ -6,3 +6,13 @@ curl -X 'POST' \
   -d '{
   "address": "7iCzEsN1xrV9gZoWMvUaWKhAhy1Cqm9iAeVAmJVThCqV"
 }'
+
+echo ""
+echo ""
+
+curl -X 'POST' \
+  'http://127.0.0.1:3000/getUserId' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "address2": "7iCzEsN1xrV9gZoWMvUaWKhAhy1Cqm9iAeVAmJVThCqV"
+}'
