9958fc874215bb922b7ad1c184f995ec3c3bc917
EC2 Default User <ec2-user@ip-172-31-25-45.eu-north-1.compute.internal>
2024-12-18 03:34:11 +0000

    release version 0.9.9

--- solana/getUserToken.sh
+++ solana/getUserToken.sh
@@ -57,11 +57,21 @@ curl -X 'POST' \
   -H 'Content-Type: application/json' \
   -d '{
   "address": "7iCzEsN1xrV9gZoWMvUaWKhAhy1Cqm9iAeVAmJVThCqV",
+  "uid": "c9fe7bf01a33e35c",
+  "signature": "397KqXkGyYkxxdBNUqDY1fpvVv7ccXFeJoDmieeawN2KPGmNbthvTPMDDcEM1NDnGTftcMf7EFnTSUoDG2Pg8VY5ZZ"
+}'
+
+echo ""
+echo ""
+
+curl -X 'POST' \
+  "http://127.0.0.1:$PORT/getUserToken" \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "address": "7iCzEsN1xrV9gZoWMvUaWKhAhy1Cqm9iAeVAmJVThCqV",
   "uid2": "c9fe7bf01a33e35c",
   "signature": "397KqXkGyYkxxdBNUqDY1fpvVv7ccXFeJoDmieeawN2KPGmNbthvTPMDDcEM1NDnGTftcMf7EFnTSUoDG2Pg8VY5"
 }'
 
 echo ""
 echo ""
-
-
